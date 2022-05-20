544d485659a54b2278f216d5cb613d2254910860
Marcella Malune <marcella.malune@gmail.com>
2022-05-20 21:21:38 +0200

    polygon setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
